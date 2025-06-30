f08f51165085f88b6ff367611766387841a1a1c8
ayarem <ayarem21@gmail.com>
2025-06-30 10:27:42 +0300

    fixes after review

--- src/main/java/com/ms/control/controllers/TestController.java
+++ src/main/java/com/ms/control/controllers/TestController.java
@@ -1,11 +1,15 @@
 package com.ms.control.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
+/**
+ * Simple test endpoint for health check or basic connectivity testing.
+ * @return "hello" as plain text response
+*/
     @GetMapping("/test")
     @ResponseBody
     public String hello() {
